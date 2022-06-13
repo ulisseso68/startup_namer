5e6e44acf420388940f268fd3a2b9c582fd4ecd2
Ulisses <campos.ulisses@gmail.com>
2022-06-12 21:57:39 -0300

    nomeador #4

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
   @override
   Widget build(BuildContext context) {
@@ -50,12 +51,29 @@ class _RandomWordsState extends State<RandomWords> {
         if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10)); /*4*/
         }
+
+        final alreadySaved = _saved.contains(_suggestions[index]);
+
         return ListTile(
-          title: Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont,
-          ),
-        );
+            title: Text(
+              _suggestions[index].asPascalCase,
+              style: _biggerFont,
+            ),
+            trailing: Icon(
+              alreadySaved ? Icons.favorite : Icons.circle_outlined,
+              color: alreadySaved ? Colors.pink : null,
+              semanticLabel:
+                  alreadySaved ? 'Descarmar' : 'Marcar como favorito',
+            ),
+            onTap: () {
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(_suggestions[index]);
+                } else {
+                  _saved.add(_suggestions[index]);
+                }
+              });
+            });
       },
     );
   }
